6f2b02f5debd62680dee459c909c4e8e30001988
Steven Chen <chensteven0617@gmail.com>
2024-08-05 17:04:56 -0700

    corrected nav menu structure

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -5,26 +5,23 @@ import { menus } from "../utils/constants";
 function NavMenu() {
   return (
     <nav className="nav-menu">
-      {menus.map((menu) => (
-        <ul key={menu.title}>
-          <li>
+      <ul>
+        {menus.map((menu) => (
+          <li key={menu.title}>
             <Link to={encodeURI(menu.path)}>{menu.title}</Link>
+            {menu.subSections && (
+              <ul className="sub-menu">
+                {menu.subSections.map((subMenu) => (
+                  <li key={subMenu.title}>
+                    <Link to={encodeURI(subMenu.path)}>{subMenu.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
-          {menu.subSections && (
-            <ul className="sub-menu">
-              {menu.subSections.map((subMenu) => (
-                <li>
-                  <Link key={subMenu.title} to={encodeURI(subMenu.path)}>
-                    {subMenu.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </ul>
-      ))}
+        ))}
+      </ul>
     </nav>
   );
 }
-
 export default NavMenu;
