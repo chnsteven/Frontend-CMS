b503f2f3fabfb66aac48836bd7f0df34b47f8755
Steven Chen <chensteven0617@gmail.com>
2024-08-12 19:46:05 -0700

    toggle menu
    user can now click on the nav menu to expand or collapse sub menus

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,102 +1,45 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { menus } from "../utils/constants";
-
-// function NavMenu() {
-//   return (
-//     <nav className="nav-menu">
-//       <ul>
-//         {menus.map((menu) => (
-//           <li key={menu.title}>
-//             <Link to={encodeURI(menu.path)}>{menu.title}</Link>
-//             {menu.subSections && (
-//               <ul className="sub-menu">
-//                 {menu.subSections.map((subMenu) => (
-//                   <li key={subMenu.title}>
-//                     <Link to={encodeURI(subMenu.path)}>{subMenu.title}</Link>
-//                   </li>
-//                 ))}
-//               </ul>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// }
-// export default NavMenu;
-
-// import React from "react";
-// import { Link, useMatch } from "react-router-dom";
-// import { menus } from "../utils/constants";
-
-// function NavMenu({ customStyles }) {
-//   const match = useMatch();
-
-//   const getActiveClass = (path) => {
-//     return match.path.includes(path) ? "active" : "";
-//   };
-
-//   return (
-//     <nav className={`nav-menu ${customStyles}`}>
-//       <ul>
-//         {menus.map((menu) => (
-//           <li key={menu.title} className={getActiveClass(menu.path)}>
-//             <Link to={encodeURI(menu.path)}>{menu.title}</Link>
-//             {menu.subSections && (
-//               <ul className="sub-menu">
-//                 {menu.subSections.map((subMenu) => (
-//                   // <li
-//                   //   key={subMenu.title}
-//                   //   className={getActiveClass(subMenu.path)}
-//                   // >
-//                   //   <Link to={encodeURI(subMenu.path)}>{subMenu.title}</Link>
-//                   // </li>
-//                   <> </>
-//                 ))}
-//               </ul>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default NavMenu;
-
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { menus } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
-function NavMenu({ customStyles }) {
+function NavMenu() {
   const location = useLocation();
+  const [activeMenu, setActiveMenu] = useState(null);
 
-  const getActiveClass = (path) => {
-    if (path === "/") {
-      return location.pathname === path ? "active" : "";
-    }
-    return location.pathname.includes(path) ? "active" : "";
+  const toggleSubMenu = (menuTitle) => {
+    setActiveMenu((prevMenu) => (prevMenu === menuTitle ? null : menuTitle));
   };
 
   return (
-    <nav className={`nav-menu ${customStyles}`}>
+    <nav className="nav-menu">
       <ul>
         {menus.map((menu) => (
-          <li key={menu.title} className={getActiveClass(menu.path)}>
-            <Link to={encodeURI(menu.path)}>{menu.title}</Link>
-            {menu.subSections && (
-              <ul className="sub-menu">
-                {menu.subSections.map((subMenu) => (
-                  <li
-                    key={subMenu.title}
-                    className={getActiveClass(subMenu.path)}
-                  >
-                    <Link to={encodeURI(subMenu.path)}>{subMenu.title}</Link>
-                  </li>
+          <li key={`nav-menu ${menu.title}`}>
+            <Link
+              to={encodeURI(menu.path)}
+              onClick={() => toggleSubMenu(menu.title)}
+            >
+              {menu.title}{" "}
+              {menu.subSections &&
+                (activeMenu === menu.title ? (
+                  <FontAwesomeIcon icon={faCaretDown} />
+                ) : (
+                  <FontAwesomeIcon icon={faCaretLeft} />
                 ))}
-              </ul>
-            )}
+            </Link>
+            {menu.subSections &&
+              location.pathname.startsWith(menu.path) &&
+              activeMenu === menu.title && (
+                <ul className="sub-menu">
+                  {menu.subSections.map((subMenu) => (
+                    <li key={`nav-sub-menu ${subMenu.title}`}>
+                      <Link to={encodeURI(subMenu.path)}>{subMenu.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
           </li>
         ))}
       </ul>
